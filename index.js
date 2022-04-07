033258c20d35779825fa342e1a23d9035fd8e84f
jorge <jorgetarifa33@gmail.com>
2022-04-07 11:12:04 -0500

    Clousure

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ let btn1 = document.getElementById('btn1'),
     btn3 = document.getElementById('btn3'),
     btn4 = document.getElementById('btn4')
 
-   //console.log(inputNum1, inputNum2);
-    //Hallar el menor de dos numeros enteros positivos
+
+//Hallar el menor de dos numeros enteros positivos
 
     btn1.addEventListener('click', ( ) => {
    let inputNum1 = Number(document.querySelector('.input1').value)
    let inputNum2 = Number(document.querySelector('.input2').value)
-   console.log(inputNum1, inputNum2);
+   //console.log(inputNum1, inputNum2);
         let result = menorNum( inputNum1, inputNum2 )
-        console.log(result);
+        alert(`El numero menor es: ${result}`);
 
     })
 
@@ -22,35 +22,57 @@ let btn1 = document.getElementById('btn1'),
         let arr = [];
         arr.push(inputNum1, inputNum2)
         let minim = Math.min(...arr)
-        console.log(minim);
+        //console.log(minim);
         return minim
     }
 
     
     //Determinar si los dos números ingresados son iguales
 
-    btn2.addEventListener('click', ( a, b) => {
-       switch(true) {
-           case a === b: 
-           alert('Los numeros son iguales')
-           break;
-           default: alert('Los numeros son diferentes')
-       }
+    btn2.addEventListener('click', ( ) => {
+      let inputNum1 = Number(document.querySelector('.input1').value)
+      let inputNum2 = Number(document.querySelector('.input2').value)
+       
+      let mayorNum = (inputNum1 > inputNum2 || inputNum1 < inputNum2) ? 'Los numeros son diferentes' : 'Los numeros son iguales'
+      alert(mayorNum)
     })
 
     
+
     //Intercambiar los valores de los dos números
     btn3.addEventListener('click', ( a,b ) => {
+    let inputNum1 = Number(document.querySelector('.input1').value)
+    let inputNum2 = Number(document.querySelector('.input2').value)
+   //console.log(inputNum1, inputNum2);
+        let result = intercambiarInput( inputNum1, inputNum2 )
+        alert(`Los numeros invertidos son: ${result[0]} y ${result[1]} `); 
         
     })
 
+    const intercambiarInput = (inputNum1, inputNum2) => {
+        let arrInputValue = []
+        arrInputValue.push(inputNum1,inputNum2 )
+        return arrInputValue.reverse()
+    }
+
+
+
+
+
+
     //Sumar los dos números
+   
+    btn4.addEventListener('click', ( ) => {
+        let inputNum1 = Number(document.querySelector('.input1').value)
+        let inputNum2 = Number(document.querySelector('.input2').value)
+        
+             let resultSuma = Sumar(inputNum1,inputNum2 )
+             alert(`La suma es: ${resultSuma}`);
+     
+         })
 
-    btn4.addEventListener('click', ( a, b) => {
-        let result= a + b;
-             alert(`La suma es ${result}`)
-    })
-
-
+         const Sumar = ( inputNum1,inputNum2 ) => {        
+           let result = inputNum2 ? Sumar(inputNum1 ^ inputNum2, (inputNum1 & inputNum2) << 1) : inputNum1;
+         return result
+    }
     
- 
