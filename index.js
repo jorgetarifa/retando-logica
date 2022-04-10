6fc4fcd4ce494475f9d16137facb2f9d356a3e52
jorge <jorgetarifa33@gmail.com>
2022-04-09 21:06:26 -0500

    End

--- index.js
+++ index.js
@@ -8,14 +8,26 @@ let btn1 = document.getElementById('btn1'),
 
 
 //Hallar el menor de dos numeros enteros positivos
-
-    btn1.addEventListener('click', ( ) => {
+    btn1.addEventListener( 'click', (e) => {
    let inputNum1 = Number(document.querySelector('.input1').value)
    let inputNum2 = Number(document.querySelector('.input2').value)
    
+   if(inputNum1 < 0  || inputNum2 < 0){
+           
+    return Swal.fire('Los numeros deben ser enteros positivos','','warning')
+}
+
    let result = menorNum(inputNum1, inputNum2)
-   let result2 = (inputNum1 === inputNum2) ? alert('Por favor ingrese numeros diferentes') :  alert(`El numero menor es: ${result}`)
-   return result2
+   let result2 = (inputNum1 === inputNum2 ) ? alert('Por favor ingrese numeros diferentes') :    Swal.fire({
+    title: `El numero menor es: ${result}`,   
+    icon: 'success',       
+    confirmButtonText: 'OK'
+  })  
+  
+   
+
+
+
 })
     const menorNum = ( a, b) => {
         let arr = [];
@@ -27,24 +39,34 @@ let btn1 = document.getElementById('btn1'),
 
     
     //Determinar si los dos números ingresados son iguales
-
-    btn2.addEventListener('click', ( ) => {
+    btn2.addEventListener( 'click', ( ) => {
       let inputNum1 = Number(document.querySelector('.input1').value)
       let inputNum2 = Number(document.querySelector('.input2').value)
-       
+      
+      if(inputNum1 < 0  || inputNum2 < 0){
+           
+        return Swal.fire('Los numeros deben ser enteros positivos','','warning')
+    }
       let mayorNum = (inputNum1 > inputNum2 || inputNum1 < inputNum2) ? 'Los numeros son diferentes' : 'Los numeros son iguales'
-      alert(mayorNum)
+      Swal.fire( mayorNum, '', 'info')
     })
 
     
 
     //Intercambiar los valores de los dos números
-    btn3.addEventListener('click', ( a,b ) => {
+    btn3.addEventListener( 'click', ( a,b ) => {
     let inputNum1 = Number(document.querySelector('.input1').value)
     let inputNum2 = Number(document.querySelector('.input2').value)
    //console.log(inputNum1, inputNum2);
+   if(inputNum1 < 0  || inputNum2 < 0){
+           
+    return Swal.fire('Los numeros deben ser enteros positivos','','warning')
+}
+        if(inputNum1 ===  inputNum2) {
+            return Swal.fire('Los numeros deben ser diferentes', '', 'warning')
+        }
         let result = intercambiarInput( inputNum1, inputNum2 )
-        alert(`Los numeros invertidos son: ${result[0]} y ${result[1]} `); 
+        Swal.fire(`Los numeros invertidos son: ${result[0]} y ${result[1]} `, '', 'success'); 
         
     })
 
@@ -56,17 +78,19 @@ let btn1 = document.getElementById('btn1'),
 
 
 
-
-
-
-    //Sumar los dos números
-   
-    btn4.addEventListener('click', ( ) => {
+    //Sumar los dos números   
+    btn4.addEventListener( 'click', ( ) => {
         let inputNum1 = Number(document.querySelector('.input1').value)
         let inputNum2 = Number(document.querySelector('.input2').value)
         
-             let resultSuma = Sumar(inputNum1,inputNum2 )
-             alert(`La suma es: ${resultSuma}`);
+
+        if(inputNum1 < 0  || inputNum2 < 0){
+           
+            return Swal.fire('Los numeros deben ser enteros positivos','','warning')
+        }
+
+        let resultSuma = Sumar(inputNum1,inputNum2 )
+        Swal.fire( `La suma es: ${resultSuma}`, '', 'success');
      
          })
 
@@ -75,3 +99,5 @@ let btn1 = document.getElementById('btn1'),
          return result
     }
     
+
+  
